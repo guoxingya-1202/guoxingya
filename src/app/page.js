656603ef283a4a4b5ba67208d2aace995083293a
guoxingya-1202 <3197616010@qq.com>
2025-05-07 12:18:56 +0800

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import  Navbar from "../zujian/Navbar";
+export default function Hello(){
+  return (
+    <div>
+       <Navbar />
+      <h1 className="text-blue-200 bg-sky-50 font-serif text-9xl text-center">Hello</h1>
+      </div>
+  )
+}
+
